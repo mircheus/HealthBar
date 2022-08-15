6a3cb9002be70a1a1ef81c324f3729aefcb2d65d
mircheus <mircheuss@yandex.ru>
2022-08-15 04:57:22 +0300

    changed event system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private float _maxHealthPoints;
     [SerializeField] private float _healthPoints;
-
-    private void Start()
-    {
-        _healthPoints = _maxHealthPoints;
-    }
+    // [SerializeField] private UnityEvent _healed;
+    // [SerializeField] private UnityEvent _damaged;
+    [SerializeField] private UnityEvent _changed;
+    
+    // private void Start()
+    // {
+    //     Debug.Log($"HP on player = {_healthPoints}");
+    // }
 
     private void Update()
-    {
+    { 
         _healthPoints = Mathf.Clamp(_healthPoints, 0, _maxHealthPoints);
     }
 
@@ -24,10 +28,16 @@ public class Player : MonoBehaviour
     public void Heal(float value)
     {
         _healthPoints += value;
+        // Debug.Log($"PLUS HP player = {HealthPoints}");
+        _changed.Invoke();
+        // _healed.Invoke();
     }
 
     public void Damage(float value)
     {
         _healthPoints -= value;
+        // Debug.Log($"MINUS HP player = {HealthPoints}; delta = {value}");
+        _changed.Invoke();
+        // _damaged.Invoke();
     }
 }
--- Assets/Scripts/SliderChanger.cs
+++ Assets/Scripts/SliderChanger.cs
@@ -15,19 +15,82 @@ public class SliderChanger : MonoBehaviour
     private float _startValue;
     private Slider _slider;
     private float target;
+    private Coroutine _changer;
 
     private void Start()
     {
         _slider = GetComponent<Slider>();
-        _slider.maxValue = _player.HealthPoints;
-        _slider.value = _slider.maxValue;
-        target = _slider.value;
+        _slider.value = _player.HealthPoints;
+        // Debug.Log($"Start HP slider = {_slider.value}");
+        // Debug.Log($"Start HP player = {_player.HealthPoints}");
+        // Debug.Log($"HP on slider = {_player.HealthPoints}");
+        // _slider.value = _slider.maxValue;
+        // target = _slider.value;
     }
     
     private void Update()
     {
-        target = _player.HealthPoints;
-        float currentValue = Mathf.SmoothDamp(_slider.value, target, ref _currentVelocity, _smoothTime * Time.deltaTime);
-        _slider.value = currentValue;
+        // target = _player.HealthPoints;
+        // float currentValue = Mathf.SmoothDamp(_slider.value, target, ref _currentVelocity, _smoothTime * Time.deltaTime);
+        // float currentValue = Mathf.MoveTowards(_slider.value, target, 1f);
+        // _slider.value = currentValue;
+    }
+
+    // public void SetPosition(float value)
+    // {
+    //     // target = _player.HealthPoints;
+    //     // float currentValue = Mathf.MoveTowards(_slider.value, target, 1f);
+    //     // _slider.value = currentValue;
+    //     // _slider.value = value;
+    // }
+
+
+    public void SlideHP()
+    {
+        _slider.value = _player.HealthPoints;
+        // Debug.Log($"slider value = {_slider.value}");
+    }
+    
+    
+    // public void IncreaseHealthBar(float value)
+    // {
+    //     if (_changer != null)
+    //     {
+    //         StopCoroutine(_changer);
+    //     }
+    //
+    //     _slider.value += value;
+    //     // _changer = StartCoroutine(MoveSlider(value));
+    // }
+    //
+    // public void DecreaseHealthBar(float value)
+    // {
+    //     if (_changer != null)
+    //     {
+    //         StopCoroutine(_changer);
+    //     }
+    //
+    //     _slider.value -= value;
+    //     // _changer = StartCoroutine(MoveSlider(value));
+    // }
+
+    private IEnumerator MoveSlider(float value)
+    {
+        var waitFor = new WaitForSeconds(0.1f);
+        float delta = 5f;
+        float target = _slider.value + value;
+        var i = 0;
+        float currentVelocity = 0f;
+        float smoothTime = 5f;
+        
+        // while (Math.Abs(_slider.value - target) > 0.1f)
+        while (_slider.value != target)
+        {
+            Debug.Log(i);
+            _slider.value = Mathf.MoveTowards(_slider.value, target, delta);
+            // _slider.value = Mathf.SmoothDamp(_slider.value, target, ref currentVelocity, smoothTime);
+            i++;
+            yield return waitFor;
+        }
     }
 }
