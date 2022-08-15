ab1fa7b9af23cfddc04eff3fcada1a184ec65ed1
mircheus <mircheuss@yandex.ru>
2022-08-16 00:47:55 +0300

    removed coroutine

--- Assets/Scripts/SliderChanger.cs
+++ Assets/Scripts/SliderChanger.cs
@@ -19,39 +19,10 @@ public class SliderChanger : MonoBehaviour
         _slider = GetComponent<Slider>();
         _slider.value = _player.HealthPoints;
     }
-    
-    // public void MoveSlider()
-    // {
-    //     float target = _player.HealthPoints;
-    //
-    //     if (_changer != null)
-    //     {
-    //         StopCoroutine(_changer);
-    //         Debug.Log($"Coroutine stopped");
-    //     }
-    //
-    //     _changer = StartCoroutine(MoveSliderCoroutine(target));
-    // }
 
     public void MoveSlider()
     {
         float target = _player.HealthPoints;
         DOTween.To(() => _slider.value, value => _slider.value = value, target, 0.5f);
-    }
-
-    private IEnumerator MoveSliderCoroutine(float value)
-    {
-        var waitFor = new WaitForSeconds(0.01f);
-        float delta = 1f;
-        float currentValue = _slider.value;
-        
-        // РАБОЧИЙ ВАРИАНТ
-        while(currentValue != value)
-        {
-            Debug.Log($"_slider.value = {_slider.value}");
-            currentValue = Mathf.MoveTowards(currentValue, value, delta);
-            _slider.value = currentValue;
-            yield return waitFor;
-        }
     }
 }
