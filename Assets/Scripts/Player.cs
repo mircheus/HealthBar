ce2734a2287fb3a2cb65a17fa8cb4494ecb28ac6
mircheus <mircheuss@yandex.ru>
2022-08-17 00:50:27 +0300

    removed Clamp method, replaced by ternary operator in Heal and Damage methods

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,23 +11,20 @@ public class Player : MonoBehaviour
     [SerializeField] private float _healthPoints;
     [SerializeField] private UnityEvent _changed;
     
-    private void Update()
-    { 
-        _healthPoints = Mathf.Clamp(_healthPoints, 0, _maxHealthPoints);
-    }
-
     public float HealthPoints => _healthPoints;
     public float MaxHealthPoints => _maxHealthPoints;
     
     public void Heal(float value)
     {
         _healthPoints += value;
+        _healthPoints = _healthPoints > _maxHealthPoints ? _maxHealthPoints : _healthPoints;
         _changed.Invoke();
     }
 
     public void Damage(float value)
     {
         _healthPoints -= value;
+        _healthPoints = _healthPoints < 0 ? 0 : _healthPoints;
         _changed.Invoke();
     }
 }
