067a0550bc0e480af7835bceebdeeda409b1f935
mircheus <mircheuss@yandex.ru>
2022-08-25 02:19:26 +0300

    removed commented block

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
-// 1. _damageButton = GetComponent<Button>(); При использовании GetComponent<>() мы должны гарантировать, что эти компоненты будут на объекте. Для этого используется RequireComponent (_healButton = GetComponent<Button>();) смысла в этих (HealButton, DamageButton) скриптах тут нету. 
-
-// 2. public static event UnityAction<float> Changed; А зачем static. Просто public event нужен. 
-
-// 3. private void OnEnable() { HealButton.Pressed += Heal; DamageButton.Pressed += Damage; } private void OnDisable() { HealButton.Pressed -= Heal; DamageButton.Pressed -= Damage; это соответственно не нужно. А private void Heal() private void Damage() Должны быть публичными, это главное взаимодействие игрока с внешним миром(его бьют или хилят) И Heal() и Damage() должны быть с параметрами величины наносимого им вреда/пользы (Heal(float heal)). 
-
-// 4. В SliderChanger поле [SerializeField] private Player _player; как раз быть должно. У нас SliderChanger жить без Player не может. Соответственно зависимость от Player _player; должна быть. 
-
-// 5. Очень хотел бы увидеть изменение _slider.DOValue(target, _slideDuration); через корунтину (не обязательно (пока что))
 
 public class Player : MonoBehaviour
 {
